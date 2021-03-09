0d0bf51659adf133906bd928ffb981c2629910c4
DEV_B <DEV_B@example.com>
2021-03-09 09:08:16 +0530

    feat(bootstrap): bootstrap config
    Keeps the change scoped to bootstrap and preserves the existing behavior.

--- microfinance/settings_local.py
+++ microfinance/settings_local.py
@@ -12,3 +12,5 @@
 # c019 2021-02-14T10:18:06 refactor initial project files
 
 # c024 2021-02-25T16:13:41 polish the deployment entrypoint
+
+# c029 2021-03-09T09:08:16 feat(bootstrap): bootstrap config
