093c24382bba5b643add0c02b5f8fe6ddcd429db
DEV_A <DEV_A@example.com>
2021-04-23 10:48:36 +0530

    tighten model wiring

--- micro_admin/admin.py
+++ micro_admin/admin.py
@@ -2,3 +2,5 @@
 # Working file
 
 # c047 2021-04-19T11:26:22 test(data-layer): admin tweaks
+
+# c049 2021-04-23T10:48:36 tighten model wiring
---
+++ micro_admin/models.py
@@ -0,0 +1,4 @@
+# micro_admin/models.py
+# Working file
+
+# c049 2021-04-23T10:48:36 tighten model wiring
