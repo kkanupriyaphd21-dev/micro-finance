4ec71a1d31cb2ccbd504ec30a412af414de4eba8
DEV_D <DEV_D@example.com>
2021-08-17 13:09:33 +0530

    verify: deployment entrypoint
    Follow-up after review. The main path is unchanged; the null case is now handled.

--- loans/views.py
+++ loans/views.py
@@ -2,3 +2,5 @@
 # Working file
 
 # c096 2021-08-08T09:25:05 verify: deployment entrypoint
+
+# c100 2021-08-17T13:09:33 verify: deployment entrypoint
--- micro_admin/views.py
+++ micro_admin/views.py
@@ -2,3 +2,5 @@
 # Working file
 
 # c093 2021-08-01T12:52:44 tighten bootstrap config
+
+# c100 2021-08-17T13:09:33 verify: deployment entrypoint
