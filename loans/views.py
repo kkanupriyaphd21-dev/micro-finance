e68b2b118254cccc7c2fabf7d72f7defd542e774
DEV_C <DEV_C@example.com>
2021-08-24 11:42:54 +0530

    wire the initial project files

--- loans/views.py
+++ loans/views.py
@@ -4,3 +4,5 @@
 # c096 2021-08-08T09:25:05 verify: deployment entrypoint
 
 # c100 2021-08-17T13:09:33 verify: deployment entrypoint
+
+# c103 2021-08-24T11:42:54 wire the initial project files
--- micro_admin/tests.py
+++ micro_admin/tests.py
@@ -2,3 +2,5 @@
 # Working file
 
 # c098 2021-08-12T10:47:19 fix(business): startup settings
+
+# c103 2021-08-24T11:42:54 wire the initial project files
